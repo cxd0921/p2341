50141b35b536e6cd7b9c0707066e23571e113c79
cxd <xiaodong.chen@tongdajs.com>
2024-12-06 11:36:56 +0800

    111111111111

--- change_slot.h
+++ change_slot.h
@@ -1,13 +1,4 @@
-/*
- * @Author: kunlong.wei@tongdajs.com
- * @Date: 2023-07-19 11:42:53
- * @LastEditors: kunlong.wei@tongdajs.com
- * @LastEditTime: 2023-07-19 14:21:25
- * @FilePath: \wkltest\test_change_slot\src\change_slot.h
- * @Description: 
- * 
- * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
- */
+
 #ifndef CHANGE_SLOT_H
 #define CHANGE_SLOT_H
 
